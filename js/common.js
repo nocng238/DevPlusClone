6e4cf7faead9a0122816cd3c0a464ec68a8cbc5b
Tu Nguyen <tu.nguyen@devplus.edu.vn>
2022-07-21 10:41:26 +0700

    common feature

---
+++ js/common.js
@@ -0,0 +1,38 @@
+const acc_btns = document.querySelectorAll(".accordion-header");
+const acc_contents = document.querySelectorAll(".answer-show");
+
+acc_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    acc_contents.forEach((acc) => {
+      if (
+        e.target.nextElementSibling !== acc &&
+        acc.classList.contains("active")
+      ) {
+        acc.classList.remove("active");
+        acc_btns.forEach((btn) => btn.classList.remove("active"));
+      }
+    });
+
+    const panel = btn.nextElementSibling;
+    panel.classList.toggle("active");
+    btn.classList.toggle("active");
+  });
+});
+
+window.onclick = (e) => {
+  if (!e.target.matches(".accordion-header")) {
+    acc_btns.forEach((btn) => btn.classList.remove("active"));
+    acc_contents.forEach((acc) => acc.classList.remove("active"));
+  }
+};
+
+/*Pop up process*/
+document.querySelector(".popup-videos").onclick = () => {
+  document.querySelector(".popup-video").style.display = "block";
+  document.querySelector("popup-video iframe").src =
+    "https://www.youtube.com/watch?v=mUjhiT0zSKI";
+};
+
+document.querySelector(".popup-video span").onclick = () => {
+  document.querySelector(".popup-video").style.display = "none";
+};
